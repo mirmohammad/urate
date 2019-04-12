b97ad8ca7467a183c1ce1e99bcc731b2de86687a
Sahil Gogna <gogna.sahil95@gmail.com>
2019-04-11 20:46:10 -0400

    base project with firebase setup

---
+++ URate-The Rating App/Pods/Target Support Files/Pods-URate-The Rating App/Pods-URate-The Rating App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_URate_The_Rating_App : NSObject
+@end
+@implementation PodsDummy_Pods_URate_The_Rating_App
+@end
---
+++ URate-The Rating App/URate-The Rating App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URate-The Rating App
+//
+//  Created by Sahil Gogna on 2019-04-11.
+//  Copyright © 2019 Sahil Gogna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
