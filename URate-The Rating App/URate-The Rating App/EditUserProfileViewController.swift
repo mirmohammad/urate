cf9359665b599f8a30452831fe81e63f93e7dd06
Ahmed Ghareeb <a.s.ghareeb@gmail.com>
2019-04-12 22:51:45 -0400

    created EditUserProfileController. test commit

---
+++ URate-The Rating App/URate-The Rating App/EditUserProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditUserProfileViewController.swift
+//  URate-The Rating App
+//
+//  Created by Ahmed Ghareeb on 2019-04-12.
+//  Copyright © 2019 Sahil Gogna. All rights reserved.
+//
+
+import UIKit
+
+class EditUserProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
