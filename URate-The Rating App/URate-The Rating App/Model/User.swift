e372c599924e750669d214d33fcfe052f30ffe24
Sahil Gogna <gogna.sahil95@gmail.com>
2019-04-14 21:43:33 -0400

    login and signup backend added

---
+++ URate-The Rating App/URate-The Rating App/Model/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  URate-The Rating App
+//
+//  Created by Sahil Gogna on 2019-04-14.
+//  Copyright © 2019 Sahil Gogna. All rights reserved.
+//
+
+import Foundation
+
+class User : NSObject{
+    //this varibale stores the User ID
+    var id: String?
+    
+    //this varibale stores User first name
+    var firstName: String?
+    
+    //this varibale stores User last name
+    var lastName: String?
+    
+    //this varibale stores User email address
+    var email: String?
+}
