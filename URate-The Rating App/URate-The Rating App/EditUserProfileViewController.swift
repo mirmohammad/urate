b2f3aa9e5a7e9e04c1da66d16ba1bf000d427e1e
Ahmed Ghareeb <a.s.ghareeb@gmail.com>
2019-04-18 20:13:32 -0400

    created the UI for user profile with editMode function

--- URate-The Rating App/URate-The Rating App/EditUserProfileViewController.swift
+++ URate-The Rating App/URate-The Rating App/EditUserProfileViewController.swift
@@ -7,24 +7,71 @@
 //
 
 import UIKit
+import Firebase
 
 class EditUserProfileViewController: UIViewController {
+    //MARK: variables
+    var ref: DatabaseReference!
+    var inEditMode:Bool = false
+    //MARK: UI variables
 
+    @IBOutlet weak var editSaveBtn: UIButton!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    //MARK: Constructor
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        ref = Database.database().reference()
+        getUserData()
+        editMode(enabled: false)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: functions
+    func getUserData(){
+       // guard
+            let uid = "JmLVLm37zJSo9HS3tgWP0LdHqSi1"//Auth.auth().currentUser?.uid else { return }
+        print(uid)
+        self.ref.child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            let userFirstName = value?["firstName"] as? String ?? "name"
+            let userLastName = value?["lastName"] as? String ?? "name"
+            let email = value?["email"] as? String ?? "email"
+            //let phone = value?["phone"] as? String ?? "phone"
+            self.nameTextField.text = userFirstName + " " + userLastName
+            self.emailTextField.text = email
+            //phoneTextField.text = phone
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        
     }
-    */
+    
+    func editMode(enabled enable:Bool){
+        nameTextField.isEnabled = enable
+        emailTextField.isEnabled = enable
+        phoneTextField.isEnabled = enable
+        inEditMode = enable
+        
+        if enable == true{
+            editSaveBtn.setTitle("Save", for: .normal)
+        }else{
+            editSaveBtn.setTitle("Edit", for: .normal)
+        }
+        
+    }
+    
+    //MARK: action 
 
+    @IBAction func saveEditClicked(_ sender: Any) {
+        switch inEditMode{
+        case true:
+            editMode(enabled: false)
+        case false:
+            editMode(enabled: true)
+        }
+    }
 }
