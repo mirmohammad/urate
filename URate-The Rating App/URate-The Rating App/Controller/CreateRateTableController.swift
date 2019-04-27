4d9111e27d56fb674936a941a86c48fbc101a3ea
William Ching <willykamus@gmail.com>
2019-04-27 18:43:05 -0400

    Added validation to generate a valid rate

--- URate-The Rating App/URate-The Rating App/Controller/CreateRateTableController.swift
+++ URate-The Rating App/URate-The Rating App/Controller/CreateRateTableController.swift
@@ -17,6 +17,10 @@ class CreateRateTableController: UITableViewController {
     var databaseHandle: DatabaseHandle?
     
     var getdep_id:String!
+    
+    var listRates = [Rate]()
+    
+    var depRates = [Rate]()
 
     
     @IBOutlet weak var userRate: CosmosView!
@@ -47,21 +51,72 @@ class CreateRateTableController: UITableViewController {
             return
         }
         
-        let rate = userRate.rating
+        fetchDepRates()
         
-        let values = ["user_id": uId, "dep_id": getdep_id!, "comment":  userComment.text ?? "", "rate" : userRate!] as [String : Any]
+        var found = false
         
-        ref?.child("Rate").childByAutoId().setValue(values) {
-            (err, ref) in
-            if err != nil {
-                print(err!)
-                return
-            } else {
-                print("Uploaded!!!")
-                self.dismiss(animated: true, completion: nil)
+        for rate in depRates {
+            
+            if rate.user_id == uId {
+                print("User already rated this department")
+                found = true
+                break
             }
+            
         }
         
+        if !found {
+            
+            let rate = self.userRate.rating
+            
+            let values = ["user_id": uId, "dep_id": self.getdep_id!, "comment":  self.userComment.text ?? "", "rate" : rate] as [String : Any]
+            
+            self.ref?.child("Rate").childByAutoId().setValue(values) {
+                (err, ref) in
+                if err != nil {
+                    print(err!)
+                    return
+                } else {
+                    print("Uploaded!!!")
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+            
+        }
+        
+    }
+    
+    func fetchDepRates () {
+        
+        let databaseLoader = DispatchQueue.global(qos: .background)
+        let loadingGroup = DispatchGroup()
+        
+        loadingGroup.enter()
+        databaseLoader.async {
+            
+            self.ref!.child("Rate").observeSingleEvent(of: .value, with: { (snapshot) in
+                guard let dict = snapshot.value as? [String:[String: Any]] else {
+                    fatalError("Bye!")
+                }
+                for (_, rate) in dict {
+                    
+                    let newRate = Rate(userID: rate["user_id"] as! String, depID: rate["dep_id"] as! String, comment: rate["comment"] as! String, rate: rate["rate"] as! Int, userName: "Empty")
+                    
+                    self.listRates.append(newRate)
+                }
+                loadingGroup.leave()
+            }, withCancel: nil)
+        }
+        
+        loadingGroup.notify(queue: .main) {
+            databaseLoader.async {
+                for rate in self.listRates {
+                    if rate.dep_id == self.getdep_id{
+                        self.depRates.append(rate)
+                    }
+                }
+            }
+        }
     }
 
 }
