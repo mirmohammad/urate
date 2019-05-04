41cf23468803359fd55e9caa77da3d8baf5a313e
Sahil Gogna <gogna.sahil95@gmail.com>
2019-05-04 04:23:56 -0400

    added landing view

---
+++ URate-The Rating App/URate-The Rating App/Controller/LandingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LandingViewController.swift
+//  URate-The Rating App
+//
+//  Created by Sahil Gogna on 2019-05-04.
+//  Copyright © 2019 Sahil Gogna. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
